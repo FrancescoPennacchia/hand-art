2100196138363a93033a1cc1c9b851d58b7a4c8a
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2021-03-25 22:31:32 +0100

    Apollo Graphql - Query Categorie HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,13 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { FirebaseAuthService } from './service/firebase-auth.service';
 
+import {HttpClientModule} from '@angular/common/http';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import { HttpHeaders } from '@angular/common/http';
+import {InMemoryCache} from '@apollo/client/core';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,12 +27,28 @@ import { FirebaseAuthService } from './service/firebase-auth.service';
     AppRoutingModule,
     IonicModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
+    HttpClientModule,
     AngularFireAuthModule
   ],
   providers: [
     FirebaseAuthService,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://metaphysics-production.artsy.net/',
+            headers: new HttpHeaders({
+              'x-access-token': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZmIxNDA4ZjIzNjQxYjAwMGU4NDAzZjEiLCJzYWx0X2hhc2giOiI1ZjYwN2Q1YWU0ODJlZDAyODBjYzY5MjYyNjZlNjVmYiIsInJvbGVzIjoidXNlciIsInBhcnRuZXJfaWRzIjpbXSwib3RwIjpmYWxzZSwiZXhwIjoxNjIwOTQzNTczLCJpYXQiOjE2MTU3NTk1NzQsImF1ZCI6IjVkNDA5OTZlNmU2MDQ5MDAwNzQ5MGZhMiIsImlzcyI6IkdyYXZpdHkiLCJqdGkiOiI2MDRlODhkNmM1M2VjYTAwMTJiY2RmODUifQ.5E0ULQbPgcOn5DxU8YjmlGE-53g5OE3fvCjIbvnGUsk'
+            })
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
     ],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
+
+const Category = gql`
+  query {
+    marketingCollections (size: 4){
+      category
+    }
+  }
+`;
 
 @Component({
   selector: 'app-home',
@@ -7,11 +17,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  data: any[];
+  loading = true;
+  error: any;
 
-  constructor() {
+  constructor(private apollo: Apollo) {
   }
 
   ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: Category,
+      })
+      .valueChanges.subscribe((result: any) => {
+      console.log('result', result.data);
+      this.data = result.data.marketingCollections;
+      this.loading = result.loading;
+      this.error = result.errors;
+    });
   }
 
 
