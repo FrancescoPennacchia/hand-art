ac69234d750632cd77c43ae33406483922596b89
Francesco <38076434+FrancescoPennacchia@users.noreply.github.com>
2021-09-22 10:06:46 +0200

    Up

--- src/app/pages/artwork-detail/artwork-detail.page.ts
+++ src/app/pages/artwork-detail/artwork-detail.page.ts
@@ -3,6 +3,11 @@ import { Location } from '@angular/common';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {ArtworkService} from '../../service/artwork.service';
 import {Artwork} from '../../model/artwork/artwork.model';
+import {Transfer} from '@ionic-native/transfer';
+import {File} from '@ionic-native/file';
+import {AlertController, Platform} from '@ionic/angular';
+
+declare var cordova: any;
 
 @Component({
   selector: 'app-artwork-detail',
@@ -13,11 +18,35 @@ export class ArtworkDetailPage implements OnInit {
 
   private idArtwork: string;
   public artwork: Artwork;
+  storageDirectory = '';
+
 
   constructor(private route: ActivatedRoute,
               private artworkService: ArtworkService,
-              private location: Location
-  ) { }
+              private location: Location,
+              public platform: Platform,
+              private transfer: Transfer,
+              private file: File,
+              public alertCtrl: AlertController
+  ) {
+    this.platform.ready().then(() => {
+      // make sure this is on a device, not an emulation (e.g. chrome tools device mode)
+      if (!this.platform.is('cordova')) {
+        return false;
+      }
+
+      if (this.platform.is('ios')) {
+        this.storageDirectory = cordova.file.documentsDirectory;
+      }
+      else if (this.platform.is('android')) {
+        this.storageDirectory = cordova.file.dataDirectory;
+      }
+      else {
+        // exit otherwise, but you could add further types here e.g. Windows
+        return false;
+      }
+    });
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
